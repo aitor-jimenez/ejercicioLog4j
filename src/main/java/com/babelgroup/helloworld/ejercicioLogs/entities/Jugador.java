a8e0410103519e54c574b5618e39179421c9c2ff
Jesús Santos <jesus.santos@babelgroup.com>
2025-03-19 12:30:11 +0100

    Jugador updated

--- src/main/java/com/babelgroup/helloworld/ejercicioLogs/entities/Jugador.java
+++ src/main/java/com/babelgroup/helloworld/ejercicioLogs/entities/Jugador.java
@@ -2,12 +2,28 @@ package com.babelgroup.helloworld.ejercicioLogs.entities;
 
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class Jugador {
 
     String name;
+    List<List<Apuesta>> apuestas;
 
-    public Jugador(String name) {
+    public Jugador(String name, List<List<Apuesta>> apuestas) {
         this.name = name;
+        this.apuestas = apuestas;
+    }
+
+    public void addApuesta(List<Apuesta> apuestas) {
+        this.apuestas.add(apuestas);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<List<Apuesta>> getApuestas() {
+        return apuestas;
     }
 }
