f040c9ecc96b6fbb318ef5acf20c6085c73b05af
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-03-19 11:26:10 +0100

    Servicio de apuestas añadido

---
+++ src/main/java/com/babelgroup/helloworld/ejercicioLogs/entities/Apuesta.java
@@ -0,0 +1,13 @@
+package com.babelgroup.helloworld.ejercicioLogs.entities;
+
+import java.util.List;
+
+public class Apuesta {
+
+    private List<Integer> apuestas;
+
+    public Apuesta(List<Integer> apuestas) {
+        this.apuestas = apuestas;
+    }
+
+}
