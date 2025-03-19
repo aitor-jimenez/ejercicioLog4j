1661215d9cbcc273e693a54c3e0a145fda8deb9b
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-03-19 11:44:09 +0100

    @Component y método getApuestas añadidos

--- src/main/java/com/babelgroup/helloworld/ejercicioLogs/entities/Apuesta.java
+++ src/main/java/com/babelgroup/helloworld/ejercicioLogs/entities/Apuesta.java
@@ -1,13 +1,20 @@
 package com.babelgroup.helloworld.ejercicioLogs.entities;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 public class Apuesta {
 
     private List<Integer> apuestas;
 
     public Apuesta(List<Integer> apuestas) {
         this.apuestas = apuestas;
+    }
+
+    public List<Integer> getApuestas() {
+        return this.apuestas;
     }
 
 }
