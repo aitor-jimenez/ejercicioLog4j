de6f67be6dd711a340df962abb191f0f8515bbb9
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-03-19 10:13:29 +0100

    Commit inicial

---
+++ src/main/java/com/babelgroup/helloworld/ejercicioLogs/EjercicioLogsApplication.java
@@ -0,0 +1,13 @@
+package com.babelgroup.helloworld.ejercicioLogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babelgroup/helloworld/ejercicioLogs/EjercicioLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babelgroup.helloworld.ejercicioLogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
