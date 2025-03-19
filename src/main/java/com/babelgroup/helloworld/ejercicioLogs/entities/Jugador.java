cd8f2e0b60672b9a584485712b9778775b87b3da
Jesús Santos <jesus.santos@babelgroup.com>
2025-03-19 11:38:29 +0100

    Sorteo implementado

--- src/main/java/com/babelgroup/helloworld/ejercicioLogs/entities/Jugador.java
+++ src/main/java/com/babelgroup/helloworld/ejercicioLogs/entities/Jugador.java
@@ -1,4 +1,13 @@
 package com.babelgroup.helloworld.ejercicioLogs.entities;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Jugador {
+
+    String name;
+
+    public Jugador(String name) {
+        this.name = name;
+    }
 }
